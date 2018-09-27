0647a7f66ef2b7d4cad9256793b7dcf099cab9c6
daurenabd <daurenabd@gmail.com>
2018-09-27 20:47:09 +0900

    Parser for bus stops

--- getArrivalInfo.js
+++ getArrivalInfo.js
@@ -2,21 +2,21 @@ const request = require('request');
 
 module.exports = {
   getArrivalInfo: function (brtNo, brtDirection, bnodeId, brtClass) {
-      let form = {
-          brtNo: brtNo,
-          brtDirection: brtDirection,
-          bnodeId: bnodeId,
-          brtClass: brtClass
-      };
+    let form = {
+      brtNo: brtNo,
+      brtDirection: brtDirection,
+      bnodeId: bnodeId,
+      brtClass: brtClass
+    };
 
-      request.post({
-          url: 'http://its.ulsan.kr/busInfo/getArrivalInfo.do',
-          form: form
-      }, function (error,response,body) {
-          if(!error && response.statusCode === 200){
-              let data = JSON.parse(body);
-              console.log(data.param.list);
-          }
-      });
+    request.post({
+      url: 'http://its.ulsan.kr/busInfo/getArrivalInfo.do',
+      form: form
+    }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        let data = JSON.parse(body);
+        console.log(data.param.list);
+      }
+    });
   }
 };
---
+++ parser/parser.js
@@ -0,0 +1,59 @@
+const request = require('request');
+const translate = require('./translate/translate');
+const url = 'http://its.ulsan.kr/busInfo/getBusstopList.do';
+
+function convert(bus) {
+  return {
+    id: bus.stopId,
+    name_kr: bus.stopName,
+    name_en: '',
+    service_id: bus.stopServiceid,
+    x: bus.stopX2,
+    y: bus.stopY2,
+  };
+}
+
+function parseAndConvert(body) {
+  const parsed = JSON.parse(body);
+  return parsed.rows.map(convert);
+}
+
+function getKorean(data) {
+  return data.map(e => e.name_kr);
+}
+
+function fillEnglish(data, english) {
+  english.forEach((text, i) => {
+    data[i].name_en = text;
+  });
+}
+
+function onSuccess(callback, body) {
+  const target = 'en';
+  let data = parseAndConvert(body);
+  const korean = getKorean(data);
+
+  translate(korean, target)
+    .then(english => {
+      fillEnglish(data, english);
+      callback(data);
+    });
+}
+
+function onError(error, response) {
+  console.log(error, response);
+}
+
+function parse(callback) {
+  return (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      onSuccess(callback, body);
+    } else {
+      onError(error, response);
+    }
+  }
+}
+
+request.post({
+  url: url,
+}, parse(console.log));
